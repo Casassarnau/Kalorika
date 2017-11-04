ee2ac70735f230d08004d28efe0f085beeedfad3
Arnau Casas <casassarnau@gmail.com>
2017-11-05 00:58:01 +0100

    debug true

--- Servidor.py
+++ Servidor.py
@@ -92,4 +92,4 @@ def ingredients():
     return json_resultat
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get("PORT", 5000)))
